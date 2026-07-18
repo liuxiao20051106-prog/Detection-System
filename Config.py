27d0465c47a0a27e5294895cca4fad3b4f765e07
GrapeYolo User <grapeyolo@example.com>
2026-07-18 13:05:51 +0800

    Initial commit: grapeYolo project

---
+++ Config.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+# 图片及视频检测结果保存路径
+save_path = 'save_data'
+
+# 使用的模型路径
+model_path = 'models/best.pt'
+
+names = {
+    0: 'immature',
+    1: 'semi-mature',
+    2: 'mature'
+}
+CH_names = [
+    '未成熟',
+    '半成熟',
+    '成熟'
+]
